8bff5c60eb2040a4d4c42b74b12074957107d375
viniciusdutra314 <dutravinisousa2@usp.br>
2025-08-15 15:06:08 +0000

    feat: basic regex parsing

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import DateTime, String
-import datetime
-from enum import Enum
+from scheme import *
+import requests
+import re
 
-class TipoRefeicao(str, Enum):
-    ALMOCO = "almoco"
-    JANTAR = "jantar"
-
-class DataBase(DeclarativeBase):
-    pass
-
-class RefeicaoRegistro(DataBase):
-    __tablename__ = "cardapios"
-    data_refeicao: Mapped[datetime.date] = mapped_column(DateTime, primary_key=True)
-    tipo_refeicao: Mapped[TipoRefeicao] = mapped_column(String(6), primary_key=True)
-    principal: Mapped[str] = mapped_column(String, nullable=False)
-    vegetariano: Mapped[str] = mapped_column(String, nullable=False)
-    guarnicao: Mapped[str] = mapped_column(String, nullable=False)
-    sobremesa_opcao1: Mapped[str] = mapped_column(String, nullable=False)
-    sobremesa_opcao2: Mapped[str] = mapped_column(String, nullable=False)
+def obter_html_cardapio()->str:
+    resp=requests.get("https://www.puspsc.usp.br/cardapio/")
+    if resp.status_code != 200:
+        raise Exception("Erro ao obter o cardápio")
+    return resp.text
 
 
-class RefeicaoIndisponivel(DataBase):
-    __tablename__ = "refeicoes_indisponiveis"
-    data_refeicao: Mapped[datetime.date] = mapped_column(DateTime, primary_key=True)
-    tipo_refeicao: Mapped[TipoRefeicao] = mapped_column(String(16), primary_key=True)
+def parsing_cardapio(html:str)->tuple[list[RefeicaoRegistro],list[RefeicaoIndisponivel]]:
+    dias_da_semana= [
+        "Segunda-feira",
+        "Terça-feira",
+        "Quarta-feira",
+        "Quinta-feira",
+        "Sexta-feira",
+        "Sábado",
+    ]
+    match_data_inicial=re.findall(r"\d{2}/\d{2}/\d{4}\s*a\s*\d{2}/\d{2}/\d{4}",html)
+    if len(match_data_inicial) != 1:
+        raise Exception("Formato de data inicial/final inválido no cardápio")
+    data_inicio = datetime.datetime.strptime(match_data_inicial[0][:10], "%d/%m/%Y").date()
+    refeicao_registros:list[RefeicaoRegistro]=[]
+    refeicao_indisponiveis:list[RefeicaoIndisponivel]=[]
+    for offset,dia in enumerate(dias_da_semana):
+        data_refeicao=data_inicio + datetime.timedelta(days=offset)
+        padrao = rf"{dia}.*?Mini Pão e Suco"
+        matches = re.findall(padrao, html, flags=re.DOTALL)
+        if not matches:
+            refeicao_indisponiveis.append(RefeicaoIndisponivel(data_refeicao=data_refeicao, tipo_refeicao=TipoRefeicao.ALMOCO))
+            continue
+        tabela=matches[0].replace("\n", "").split("<br />")[1:-1]
+        sobremesa_a,sobremesa_b = tabela[-1].replace("Sobremesa: ","").rsplit(r"/",1)
+        refeicao_registros.append(RefeicaoRegistro(
+            data_refeicao= data_refeicao,
+            tipo_refeicao=TipoRefeicao.ALMOCO,
+            principal=tabela[0],
+            vegetariano=tabela[1].replace("Opção do Prato Principal: ", ""),
+            guarnicao=tabela[2],
+            sobremesa_opcao1=sobremesa_a,
+            sobremesa_opcao2=sobremesa_b
+        ))
+    return. (refeicao_registros,refeicao_indisponiveis)
 
 
+parsing_cardapio(obter_html_cardapio())
---
+++ scheme.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, String
+import datetime
+from enum import Enum
+
+class TipoRefeicao(str, Enum):
+    ALMOCO = "almoco"
+    JANTAR = "jantar"
+
+class ReprMixin:
+    def __repr__(self):
+        attrs = []
+        for column in self.__table__.columns:
+            val = getattr(self, column.name)
+            attrs.append(f"{column.name}={val!r}")
+        return f"<{self.__class__.__name__}({', '.join(attrs)})>"
+
+
+class DataBase(DeclarativeBase,ReprMixin):
+    pass
+
+class RefeicaoRegistro(DataBase):
+    __tablename__ = "cardapios"
+    data_refeicao: Mapped[datetime.date] = mapped_column(DateTime, primary_key=True)
+    tipo_refeicao: Mapped[TipoRefeicao] = mapped_column(String(6), primary_key=True)
+    principal: Mapped[str] = mapped_column(String, nullable=False)
+    vegetariano: Mapped[str] = mapped_column(String, nullable=False)
+    guarnicao: Mapped[str] = mapped_column(String, nullable=False)
+    sobremesa_opcao1: Mapped[str] = mapped_column(String, nullable=False)
+    sobremesa_opcao2: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class RefeicaoIndisponivel(DataBase):
+    __tablename__ = "refeicoes_indisponiveis"
+    data_refeicao: Mapped[datetime.date] = mapped_column(DateTime, primary_key=True)
+    tipo_refeicao: Mapped[TipoRefeicao] = mapped_column(String(6), primary_key=True)
+
+
